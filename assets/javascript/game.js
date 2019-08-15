4f4edcea49549d5455a9f89010d2088a9dcf4c36
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-08-15 08:36:32 -0700

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
+
+// Variables 
 var monsterHunter = ("diablos","nargacuga","zinogre","deviljo","plesioth");
 
+// Empty variables to store for later.
 var selectedWord = "";
+var lettersOfWord = [];
+var blanks = 0;
+var blankAndCorrect = [];
+var wrongGuess = [];
+
+//Counter variables
+var wins = 0;
+var loses = 0;
+var guesses = 11; 
