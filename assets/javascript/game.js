8a0b8713637ff4be10e5c57a9b4f6b48df0af5ec
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-08-15 09:58:53 -0700

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 // Variables 
-var monsterHunter = ("diablos","nargacuga","zinogre","deviljo","plesioth");
+var monsterHunter = ["diablos","nargacuga","zinogre","deviljo","plesioth"]
 
 // Empty variables to store for later.
 var selectedWord = "";
@@ -11,5 +11,93 @@ var wrongGuess = [];
 
 //Counter variables
 var wins = 0;
-var loses = 0;
-var guesses = 11; 
+var losses = 0;
+var guessesRemaining = 11; 
+
+// getting the game started
+
+function Game() {
+  // this is to generate a random word from array.
+  selectedWord = monsterHunter[Math.floor(Math.random() * monsterHunter.length)];
+
+  lettersOfWord = selectedWord.split("");
+
+  blanks = lettersOfWord.length;
+
+  for (var i = 0; i < blanks; i++){
+    blankAndCorrect.push("_");
+  }
+
+  document.getElementById("currentWord").innerHTML = " " + blankAndCorrect.join(" ");
+
+  console.log(selectedWord);
+  console.log(lettersOfWord);
+  console.log(blanks);
+  console.log(blankAndCorrect);
+}
+
+// Creating a reset for game
+
+function reset() {
+  guessesRemaining = 11;
+  wrongGuess = [];
+  blankAndCorrect = [];
+  Game()
+}
+
+function checkLetters(letter){
+  var letterInWord = false;
+
+  for (var i = 0; i < blanks; i++){
+    if(selectedWord[i] === letter) {
+      letterInWord = true;
+    }
+  }
+
+  if (letterInWord) {
+    for (var i = 0; i < blanks; i++){
+      if(selectedWord[i] == letter){
+        blankAndCorrect[i] = letter;
+      }
+    }
+  }
+
+  else {
+    wrongGuess.push(letter);
+    guessesRemaining--;
+  }
+  console.log(blankAndCorrect);
+}
+
+function complete() {
+  console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+
+  if (lettersOfWord.toString() == blankAndCorrect.toString()){
+    wins++;
+    reset()
+
+    document.getElementById("win-counter").innerHTML = " " + wins;
+  }
+  else if (guessesRemaining === 0) {
+    losses++;
+    reset()
+    document.getElementById("lose-counter").innerHTML = " " + losses;
+   }
+   
+   document.getElementById("currentword").innerHTML = "  " + blankAndCorrect.join(" ");
+   document.getElementById("guesses-remaining").innerHTML = " " + guessesRemaining;
+}
+
+Game()
+
+document.onkeyup = function (event) {
+  var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+
+  checkLetters(guesses);
+
+  complete();
+
+  console.log(guesses);
+
+  document.getElementById("playerGuesses").innerHTML = " " + wrongGuess.join(" ");
+}
